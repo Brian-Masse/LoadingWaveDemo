6ca3d1eb50b06c807f9a84c41c65e9a57631ff00
Brian Masse <brianm25it@gmail.com>
2024-05-30 02:03:54 -0700

    initial commit

---
+++ WaveLoadingDemo/WaveLoadingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveLoadingDemoApp.swift
+//  WaveLoadingDemo
+//
+//  Created by Brian Masse on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct WaveLoadingDemoApp: App {
+    var body: some Scene {
+        WindowGroup { 
+            ContentView()
+        }
+    }
+}
